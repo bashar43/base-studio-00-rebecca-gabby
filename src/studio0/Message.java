b17f4afa269549666524d2d07986f93e247db927
gabriellebarthelmy <gabriellebarthelmy@localhost>
2023-08-31 12:25:57 -0500

    Today we worked on studio 0. We made soem changes to the code. i.e we changed 6h

--- src/studio0/Message.java
+++ src/studio0/Message.java
@@ -3,6 +3,6 @@ package studio0;
 public class Message {
 
 	public static void main(String args[]) {
-		System.out.println("Say hi to your group mates!");
+		System.out.println("Say hi to your group mates! Hi group mates! nice to meet you! We're gonna be academic weapons this semester!");
 	}
 }
